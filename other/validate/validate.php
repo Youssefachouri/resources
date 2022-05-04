e2af1ac766884bbc5bc6324c9e1a40998d22186d
Marina Glancy <marina.glancy@gmail.com>
2022-05-04 20:22:34 +0200

    Autofix string validation

--- other/validate/validate.php
+++ other/validate/validate.php
@@ -3,12 +3,13 @@
 require_once(__DIR__.'/helper.php');
 
 if (count($argv) < 2) {
-    echo "Usage: php ".basename(__FILE__)." LANG [SECTION [FILENAME]]\n";
+    echo "Usage: php ".basename(__FILE__)." LANG [SECTION [FILENAME]] [--autofix]\n";
     exit(1);
 }
 
 $maindir = dirname(dirname(__DIR__));
-[$script, $language, $section, $filename] = array_merge($argv, [null, null, null]);
+$autofix = in_array("--autofix", $argv);
+[$script, $language, $section, $filename] = array_merge(array_diff($argv, ['--autofix']), [null, null, null]);
 
 
 $errors = [];
@@ -43,7 +44,7 @@ function comparefolder($language, $section): array {
 }
 
 function comparefiles($language, $section, $filename): array {
-    global $maindir;
+    global $maindir, $autofix;
     $enfile = "texts/en/{$section}/{$filename}";
     $langfile = "texts/{$language}/{$section}/{$filename}";
     $errors = [];
@@ -70,23 +71,37 @@ function comparefiles($language, $section, $filename): array {
         // These were fatal errors - return now.
         return $errors;
     }
+    $canfix = true;
 
     // Compare parts of the file.
     $missing = array_diff(array_keys($endata), array_keys($langdata));
     if (count($missing)) {
         $errors[] = "File {$langfile} does not have section(s): ".join(', ', $missing);
+        $canfix = false;
     }
 
     $extra = array_diff(array_keys($langdata), array_keys($endata), ['Previous titles']);
     if (count($extra)) {
         $errors[] = "File {$langfile} has extra section(s): ".join(', ', $extra);
+        $canfix = false;
     }
 
     // Compare each part.
-    $parts = array_diff(array_keys($endata), ['h1logn', 'h1short'], $missing);
+    $parts = array_diff(array_keys($endata), ['h1long', 'h1short'], $missing);
     foreach ($parts as $part) {
+        $langdata[$part] = $langdata[$part] ?? '';
         $errors = array_merge($errors,
-            comparetext($langfile, $section, $part, $langoffsets[$part] ?? 0, $endata[$part] ?? '', $langdata[$part] ?? ''));
+            comparetext($langfile, $section, $part, $langoffsets[$part] ?? 0, $endata[$part] ?? '', $langdata[$part], $autofix));
+    }
+
+    if ($canfix && $autofix && count($errors)) {
+        //print_r($langdata);
+        //print_r(array_keys($endata));
+        $text = array_key_exists('h1short', $langdata) ? "<h1>[{$langdata['h1short']}] {$langdata['h1long']}</h1>\n\n" : "<h1>{$langdata['h1long']}</h1>\n\n";
+        foreach ($langdata as $header => $content) if (!in_array($header, ['h1short', 'h1long', 'main'])) $text .= "--- $header ---\n\n".trim($content)."\n\n";
+        $text .= "----------\n\n".trim($langdata['main']);
+        //echo "\n\n\n$text\n\n";
+        file_put_contents($langfile, trim($text)."\n");
     }
 
     return $errors;
